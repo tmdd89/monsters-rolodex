e59aa9288843b8876dce21ae35837eb482f3b06b
Tai <taimoor.m89@example.com>
2020-01-21 17:32:47 -0500

    Updates

---
+++ precache-manifest.f8cae941dee73e31cbf16ab74b1ada75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c363be1608e105fbd74386fd3fc9b2ae",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e61bd5017e98c64fdd4f",
+    "url": "/monsters-rolodex/static/css/main.5672ff22.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e61bd5017e98c64fdd4f",
+    "url": "/monsters-rolodex/static/js/main.b6a54665.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
